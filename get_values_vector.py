361d1400275d78d6a8f57bb692e1ab066017dcf1
AlanaEng <54325392+AlanaEng@users.noreply.github.com>
2020-09-27 11:07:11 -0300

    Create get_values_vector.py

---
+++ get_values_vector.py
@@ -0,0 +1,15 @@
+df = ['casa', 'carro', 'pessoa', 'idade', 'religiao', 'teste', 'cidade']
+
+firstValue = df[0]
+lastValue = df[-1:]
+fourLastValues = df[-4:]
+fourFirstValues = df[:4]
+fifthValue = df[-3]
+secondValue = df[-6]
+
+print("\nPrmeiro valor: " + str(firstValue))
+print("\nÚltimo valor: ", str(lastValue))
+print("\nÚltimos quatro valores: ", str(fourLastValues))
+print("\nPrimeiros quatro valores: ", str(fourFirstValues))
+print("\nQuinto valor: ", str(fifthValue))
+print("\nSegundo valor: ", str(secondValue))
